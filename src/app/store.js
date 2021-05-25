783d9b975aac4de293794982ced66785dab471f4
yongsokheng <yongsokheng@gmail.com>
2021-05-25 13:16:12 +0700

    Add list task

--- src/app/store.js
+++ src/app/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 import counterReducer from '../features/counter/counterSlice';
 import taskReducer from '../features/task/taskSlice';
+import boardReducer from '../features/board/boardSlice';
 
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
-    task: taskReducer
+    task: taskReducer,
+    board: boardReducer
   },
 });
